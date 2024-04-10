e03a5de27f1c6659f5152b4012da56d7035a7b25
Konstantin Makarov <kostyamakarov14@gmail.com>
2024-04-10 16:18:04 +0500

    solve hw2

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
-#include "mainwindow.h"
-
-#include <QApplication>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+#if defined(Q_OS_WIN)
+    //QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+
+    engine.load(QUrl(QStringLiteral("qrc:/qt/qml/qtquickapplication3/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
 }
